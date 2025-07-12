7d766ab535d958986669e1910fd4eede3d54fdf1
ziyin ab. <ziyinab00@gmail.com>
2025-07-12 11:51:17 +0300

    adjusted

--- basic_operations.py
+++ basic_operations.py
@@ -1,7 +1,8 @@
+"""Basic arithmetic operations demonstration."""
+
 number1 = 10
 number2 = 5
 
-print(number1 + number2);
-print (number1 - number2);
-print (number1 * number2);
-print (number1 / number2);
+print(f"sum of {number1} and {number2} is {number1 + number2}")
+print(f"difference of {number1} and {number2} is {number1 - number2}")
+print(f"product of {number1} and {number2} is {number1 * number2}")
