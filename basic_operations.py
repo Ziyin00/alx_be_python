76c8922acdc26983855110eea5c15154feedff26
ziyin ab. <ziyinab00@gmail.com>
2025-07-12 12:26:23 +0300

    made two tasks

--- basic_operations.py
+++ basic_operations.py
@@ -3,6 +3,6 @@
 NUMBER1 = 10
 NUMBER2 = 5
 
-print(f"sum of {NUMBER1} and {NUMBER2} is {NUMBER1 + NUMBER2}")
-print(f"difference of {NUMBER1} and {NUMBER2} is {NUMBER1 - NUMBER2}")
-print(f"product of {NUMBER1} and {NUMBER2} is {NUMBER1 * NUMBER2}")
+print(f"Addition  of {NUMBER1} and {NUMBER2} is {NUMBER1 + NUMBER2}")
+print(f"Subtraction  of {NUMBER1} and {NUMBER2} is {NUMBER1 - NUMBER2}")
+print(f"Multiplication of {NUMBER1} and {NUMBER2} is {NUMBER1 * NUMBER2}")
---
+++ rectangle_area.py
@@ -0,0 +1,8 @@
+"""Rectangle area calculation."""
+
+LENGTH = 10
+WIDTH = 5
+
+AREA = LENGTH * WIDTH
+
+print(f"The area of the rectangle is: {AREA}")
